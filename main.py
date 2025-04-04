32ff70f89cb2bbfbc55201e7e183005718567703
zelalem-t8 <zelalem.t8@gmail.com>
2025-04-04 06:24:44 -0700

    update on models

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
-from fastapi import FastAPI,Query
-from pydantic import BaseModel
-from typing import Annotated
+from fastapi import FastAPI,Query,Path
+from pydantic import BaseModel,Field
+from typing import Annotated,Literal
 
 import uvicorn
 app = FastAPI()
+class Image(BaseModel):
+    url:str
+    name:str|None = None
+    model_config={
+        "json_schema_extra":{
+            "examples":[{
+                "url":"https://example.com/image.png",
+                "name":"example"
+            }]
+        }
+    }
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool|None = None
+    images: list[Image]|None = None
+    model_config={
+        "json_schema_extra":{
+            "examples":[{
+                "name":"cosmetic",
+                "price":100.0,
+                "is_offer":True
+            }]
+        }
+    }
+class FilterParams(BaseModel):
+    limit:int=Field(100,ge=0,le=1000)
+    offset:int=Field(0,ge=0,le=1000)
+    tags:list[str]|None = []
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    model_config={
+        "json_schema_extra": {
+            "example":{
+                "limit": 100,
+                "offset": 0,
+                "tags": ["tag1", "tag2"],
+                "order_by": "created_at"
+            }
+        }
+
+    }
+
+    
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Annotated[str|None,Query(min_length=10)] = None)->Item:
+async def read_item(item_id: Annotated[int,Path(title="The item id to be retrived")], q: Annotated[FilterParams,Query()] = None)->Item:
     item = Item(name="Item Name", price=100.0, is_offer=True)
     if q:
         item.name += f" - {q}"
